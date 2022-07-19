b32c410b567aa3e1b6f59f7d87a9dfff30433f36
Limezest <cyril@sabourau.lt>
2022-07-19 10:40:33 +0200

    pass prettier

--- content/_data/siteconfig.js
+++ content/_data/siteconfig.js
@@ -23,25 +23,25 @@ module.exports = {
     socialLinkedIn: "cyrilsabourault",
     // Google Analytics ID  (optional, remove it not needed), used for... well, Google Analytics
     googleAnalytics: "UA-97440152-1"
-        // Algolia-powered search  (optional, remove it not needed),
-        // See: https://github.com/algolia/algoliasearch-netlify
-        // algoliaSearch: {
-        //     // When enabled shows the search bar in the UI
-        //     enabled: true,
-        //     // You'll have to set this manually in your build settings.
-        //     // The value comes from Algolia, and is either visible in the
-        //     // UI for the Crawler Plugin or the Algolia Dashboard.
-        //     appId: process.env.ALGOLIA_APP_ID,
-        //     // You'll have to set this manually in your build settings.
-        //     // The value comes from Algolia, and is either visible in the
-        //     // UI for the Crawler Plugin or the Algolia Dashboard.
-        //     searchApiKey: process.env.ALGOLIA_SEARCH_API_KEY,
-        //     // You'll have to set this manually in your build settings.
-        //     // The value comes from Algolia, and is either visible in the
-        //     // UI for the Crawler Plugin or the Algolia Dashboard.
-        //     siteId: process.env.ALGOLIA_SITE_ID,
-        //     // Assuming that you deploy your "main" branch. Otherwise you
-        //     // can either override or configure this (using process.env.HEAD)
-        //     branch: "main"
-        // }
+    // Algolia-powered search  (optional, remove it not needed),
+    // See: https://github.com/algolia/algoliasearch-netlify
+    // algoliaSearch: {
+    //     // When enabled shows the search bar in the UI
+    //     enabled: true,
+    //     // You'll have to set this manually in your build settings.
+    //     // The value comes from Algolia, and is either visible in the
+    //     // UI for the Crawler Plugin or the Algolia Dashboard.
+    //     appId: process.env.ALGOLIA_APP_ID,
+    //     // You'll have to set this manually in your build settings.
+    //     // The value comes from Algolia, and is either visible in the
+    //     // UI for the Crawler Plugin or the Algolia Dashboard.
+    //     searchApiKey: process.env.ALGOLIA_SEARCH_API_KEY,
+    //     // You'll have to set this manually in your build settings.
+    //     // The value comes from Algolia, and is either visible in the
+    //     // UI for the Crawler Plugin or the Algolia Dashboard.
+    //     siteId: process.env.ALGOLIA_SITE_ID,
+    //     // Assuming that you deploy your "main" branch. Otherwise you
+    //     // can either override or configure this (using process.env.HEAD)
+    //     branch: "main"
+    // }
 };
